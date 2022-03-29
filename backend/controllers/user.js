1578c09da816acc33596fd51773928315af069e5
Aurélien.M <a.montepin@yahoo.com>
2022-03-29 11:14:55 +0200

    1 erreur 403 a 401

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -27,13 +27,13 @@ exports.login = (req, res, next) => {
   User.findOne({ email: req.body.email })
     .then(user => {
       if (!user) {
-        return res.status(401).json({ error: 'Utilisateur non trouvé !' });
+        return res.status(403).json({ message: 'Email incorrect' });
       }
       bcrypt.compare(req.body.password, user.password)
         .then(valid => {
           if (!valid) {
-            return res.status(401).json({ error: 'Mot de passe incorrect !' });
-          }
+            res.status(401).json({ message: 'Mot de passe incorrect' });
+          } 
           res.status(200).json({
             userId: user._id,
             token: jwt.sign(
